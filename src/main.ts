0ac0c9946ced00598780d0b110d035f59764c916
Malin Jansson <mallanzzz@Malins-MBP.home>
2024-10-21 17:28:19 +0200

    created objects and a list

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
+import { toDoList } from "./models/toDoList";
 import "./style.css";
+
+const taskOne = new toDoList ("Handla mat");
+const taskTwo = new toDoList ("Storstäda");
+const taskThree = new toDoList ("Laga matlådor");
+const taskFour = new toDoList ("Styrketräna");
+const taskFive = new toDoList ("Tvätta kläder");
+const taskSix = new toDoList ("Ta en promenad");
+const taskSeven = new toDoList ("Middag med vänner");
+const taskEight = new toDoList ("Koda en hemsida"); 
+
+const myList: toDoList[] = [taskOne, taskTwo, taskThree, taskFour,taskFive, taskSix, taskSeven, taskEight];
